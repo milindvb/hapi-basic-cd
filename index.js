abef168d2d5bccc8e0d76e233326403e51b8ac1e
milindvb <milind141@yahoo.com>
2018-08-20 16:59:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var hapi = require('hapi');
 var server = new hapi.Server();
 
@@ -22,3 +23,27 @@ if (!module.parent) {
 }
 
 module.exports = server;
+*/
+
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+const init = async () => {
+
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
