0ff63d19ad9900a8d7f8931bd3e12f4765552d1b
milindvb <milind141@yahoo.com>
2018-08-20 18:11:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,37 +15,24 @@ server.route({
 });
 
 server.route({
-     method: 'POST',
-     path: '/submit',
-     config: { 	 
-     	payload: {
-         	output: 'stream',
-         	parse: true,
-         	allow: 'multipart/form-data',
-            maxBytes: 1000000000 
-     	},    	 
-        handler: function (request, reply) {
-             	var data = request.payload;
-             	if (data.file) {
-                 	var name = data.file.hapi.filename;
-                 	var path = __dirname + "/uploads/" + name;
-                 	var file = fs.createWriteStream(path);
-                 	 
-                 	file.on('error', function (err) { 
-                 	    console.error(err) 
-                 	});
-                 	 
-                 	data.file.pipe(file);
-                 	 
-                 	data.file.on('end', function (err) { 
-                     	var ret = {
-                         	filename: data.file.hapi.filename,
-                         	headers: data.file.hapi.headers
-                     	}
-                 	    reply(JSON.stringify(ret));
-                 	})
-             	}
+    method: "POST",
+    path: "/submit",
+    config: {
+        payload: {
+            output: "stream",
+            parse: true,
+            allow: "multipart/form-data",
+            maxBytes: 2147483648,
+            timeout: false
         }
+    },
+    handler: (request, response) => {
+        var result = [];
+        for(var i = 0; i < request.payload["file"].length; i++) {
+            result.push(request.payload["file"][i].hapi);
+            request.payload["file"][i].pipe(fs.createWriteStream(__dirname + "/uploads/" + request.payload["file"][i].hapi.filename))
+        }
+        response(result);
     }
 });
 
